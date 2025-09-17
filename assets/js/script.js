72be1559c8cca3c1f0d1715a9a506dede1d9b691
TuNuevoUsuario <maysilvaespinoza@crackthecode.la>
2025-09-17 09:13:05 -0500

    agregando los datos de las mascotas en javascrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,63 @@ let dogsData = [
         id: 1,
         imgUrl: "../img/perrito1.jpg",
         name: "Firulais",
-    }
+    },
+    {
+        id: 2,
+        imgUrl: "../img/perrito2.jpg",
+        name: "Max",        
+    },
+    {
+        id: 3,
+        imgUrl: "../img/perrito3.jpg",  
+        name: "Rocky",
+    },
+    {
+        id: 4,
+        imgUrl: "../img/perrito4.jpg", 
+        name: "Rocky",     
+    },
+    {
+        id: 5,
+        imgUrl: "../img/perrito5.jpg", 
+        name: "Luna",     
+    },
+    {
+        id: 6,
+        imgUrl: "../img/perrito6.jpg",
+        name: "Mia",
+    },
+    {
+        id: 7,  
+        imgUrl: "../img/perrito7.jpg",
+        name: "Coco",
+    },
+    {
+        id: 8,
+        imgUrl: "../img/perrito8.jpg",
+        name: "Mango",
+    },
+    {
+        id: 9,  
+        imgUrl: "../img/perrito9.jpg",
+        name: "Casper",
+    },
+    {
+        id: 10, 
+        imgUrl: "../img/perrito10.jpg",
+        name: "Kira",
+    },
+    {
+        id: 11,
+        imgUrl: "../img/perrito11.jpg",
+        name: "Bob",
+    },
+    {
+        id: 12,
+        imgUrl: "../img/perrito12.jpg",
+        name: "Toby",
+    },
+
 ]
 
 document.querySelectorAll(".dog-btn").forEach(button => {
@@ -14,3 +70,27 @@ document.querySelectorAll(".dog-btn").forEach(button => {
     })
 })
 
+let cardContainer = document.getElementById("dog-card-container");
+
+for (let i = 0; i < dogsData.length; i++) {
+    cardContainer.innerHTML += `
+        <div class="col-12 col-md-3">
+            <div class="card perri">
+                <img
+                    src="${dogsData[i].imgUrl}"
+                    class="card-img-top"
+                    alt="${dogsData[i].name}"
+                />
+                <div class="card-body text-center">
+                <h5 class="card-title">${dogsData[i].name}</h5>
+                <a
+                    href="./assets/html/mascotas.html"
+                    class="btn btn-primary dog-btn"
+                    id="perro-${dogsData[i].id}"
+                    >Adoptame</a
+                >
+                </div>
+            </div>
+        </div>
+    `;
+}
